9d8dee5c412a6a812c84ef8e110b2148146b7683
Paul Savignano <Paul.Savignano@gmail.com>
2016-11-09 18:00:14 -0800

    Create goToStore method.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
+import { getFunName } from '../helpers'
 
 class StorePicker extends Component {
+  constructor() {
+    super()
+    this.goToStore = this.goToStore.bind(this)
+  }
+  goToStore(event) {
+    event.preventDefault()
+    const storeId = this.storeInput.value
+    this.context.router.transitionTo(`/store/${storeId}`)
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={ this.goToStore }>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name"/>
+        <input
+          type="text"
+          ref={ (input) => { this.storeInput = input } }
+          required
+          placeholder="Store Name"
+          defaultValue={ getFunName() }
+        />
         <button type="submit">Visit Store -></button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object,
 }
 
 export default StorePicker
