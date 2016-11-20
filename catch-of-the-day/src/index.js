d42692fa7d8b53c8ee90e67d653aedf91b5e3527
Paul Savignano <Paul.Savignano@gmail.com>
2016-11-20 13:26:23 -0800

    Add Github pages deployment.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -6,9 +6,11 @@ import StorePicker from './components/StorePicker'
 import App from './components/App'
 import NotFound from './components/NotFound'
 
+const repo = `/${window.location.pathname.split('/')[1]}`
+
 const Root = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={ repo }>
       <div>
         <Match exactly pattern="/" component={ StorePicker }/>
         <Match pattern="/store/:storeId" component={ App }/>
